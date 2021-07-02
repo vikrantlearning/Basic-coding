82908dd490aba8b9d1b926e0492e6c257ac96b8e
vikrantlearning <52483174+vikrantlearning@users.noreply.github.com>
2021-07-02 21:55:52 +0530

    Add files via upload

---
+++ Most_frequently_occuring__string.cpp
@@ -0,0 +1,27 @@
+//mostfrequently occuring
+string mostFrequentWord(string arr[], int n)
+    {
+        // code here
+        map<string,int>mp;
+        map<string,int>mp1;
+        int fi[n]={0};
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+            if(mp[arr[i]]==1)
+            {
+                fi[i]=1;
+            }
+        }
+        int max=-1;
+        string s;
+        for(int i=0;i<n;i++)
+        {
+            if(mp[arr[i]]>=max&&fi[i])
+            {
+                s=arr[i];
+                max=mp[arr[i]];
+            }
+        }
+        return s;
+    }
