2cc2a17718cafb972e59c5a68537301d5577cfba
vikrantlearning <52483174+vikrantlearning@users.noreply.github.com>
2022-01-22 21:21:20 +0530

    Add files via upload

---
+++ min_repeats.cpp
@@ -0,0 +1,28 @@
+//Number of Min Repeats of A such that B becomes substring of A.
+
+int minRepeats(string A, string B) {
+        // code here
+        int m=A.length();
+        int n=B.length();
+        int count;
+        //bool found=false;
+        for(int i=0;i<m;i++)
+        {
+          int j=i;
+          count=1;
+          int k=0;
+          while(k<n&&A[j]==B[k])
+          {
+              if(k==(n-1))
+               return count;
+              
+              j=(j+1)%m;
+              
+              if(j==0)
+               count++;
+               
+              k++;
+          }
+        }
+        return -1;
+    }
